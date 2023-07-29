d4932a541ffd5553e05f3337e714249914236752
Rado-Andri <andriamamonjisoan16@gmail.com>
2023-07-29 17:50:39 +0200

    exo

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(users => {
+        let columnsHtml = "<div class='user-columns'>";
+        users.forEach((element, index) => {
+            let imagePath = `./photo_users/${index + 1}.jpg`; // Chemin d'accès à l'image correspondant à l'utilisateur
+            let column = `
+                <div class='user-column'>
+                    <img src='${imagePath}' alt='${element.username}' >
+                    <h2>${element.username}</h2>
+                    <p><strong>Name:</strong> ${element.name}</p>
+                    <p><strong>Email:</strong> ${element.email}</p>
+                    <p><strong>Address:</strong> ${element.address.street}</p>
+                    <p><strong>Contact:</strong> ${element.phone}</p>
+                    <p><strong>Website:</strong> ${element.website}</p>
+                    <p><strong>Company:</strong> ${element.company.name}</p>
+                </div>
+            `;
+            columnsHtml += column;
+        });
+        columnsHtml += "</div>";
+
+        document.getElementById("users-list").innerHTML = columnsHtml;
+    })
+    .catch(error => console.error(error));
